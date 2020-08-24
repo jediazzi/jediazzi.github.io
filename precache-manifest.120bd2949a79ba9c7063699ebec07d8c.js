c09581417d7529974f8032210c516d08799528e1
jediazzi <jefferson_diazzi@hotmail.com>
2020-08-24 12:40:16 -0300

    arquivos adicionados

---
+++ precache-manifest.120bd2949a79ba9c7063699ebec07d8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295d0872f6ca019a0c4708cd307b9cc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fb37f0c4203d81c5ce6",
+    "url": "/static/css/main.8b887918.chunk.css"
+  },
+  {
+    "revision": "d7c3b5a4583a7f6f260a",
+    "url": "/static/js/2.26f0b594.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.26f0b594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb37f0c4203d81c5ce6",
+    "url": "/static/js/main.d198ffa1.chunk.js"
+  },
+  {
+    "revision": "e3d70c3c2f539355d46c",
+    "url": "/static/js/runtime-main.6c2194b5.js"
+  }
+]);
